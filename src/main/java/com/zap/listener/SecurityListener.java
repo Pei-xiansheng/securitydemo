e46d22608006a86691f342bf36540c064fd68a05
Apeng <1641984918@qq.com>
2022-09-06 14:10:52 +0800

    fix

--- src/main/java/com/zap/listener/SecurityListener.java
+++ src/main/java/com/zap/listener/SecurityListener.java
@@ -16,12 +16,16 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class SecurityListener {
 
+    @Value("${kafka.consumer.group.topic}")
+    private String kafkaTopic;
+
     @Value("${kafka.consumer.group.id}")
     private String kafkaGroupId;
+
     @Value("${kafka.consumer.group.factory}")
     private String kafkaFactory;
 
-    @KafkaListener(id = "securityListener", groupId = "${kafka.consumer.group.id}", topics = "ap_kafka_test", containerFactory = "${kafka.consumer.group.factory}")
+    @KafkaListener(id = "securityListener", groupId = "${kafka.consumer.group.id}", topics = "kafka.consumer.group.topic", containerFactory = "${kafka.consumer.group.factory}")
     public void listener(ConsumerRecord<String, String> record) {
         try {
             dealMsg(record);
